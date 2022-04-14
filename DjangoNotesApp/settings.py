95d4ec7ed61ba842797fe5ef02ac4ae565960e59
Henry Anthony <henrykay@GORDONMAC.local>
2022-04-15 02:41:35 +0800

    settings edit

--- DjangoNotesApp/settings.py
+++ DjangoNotesApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0m(v--eq&plv#pn6d)3-)e&+qh8vibc5wod&u6kwah0(gr3lj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-notes-app.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
